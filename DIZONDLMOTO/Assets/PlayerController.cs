88e447b65dd50de65d870063203a9dc74ffe0a8d
LAPTOP-6B4D7S5I\Mike <dizon2295@gmail.com>
2024-03-19 10:12:38 +0800

    First Commit

---
+++ DIZONDLMOTO/Assets/PlayerController.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+    public SwordAttack swordAttack;
+
+
+    Vector2 movementInput;
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rb;
+    Animator animator;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    bool canMove = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+
+
+    private void FixedUpdate()
+    {
+        if (canMove)
+        {
+            
+            if (movementInput != Vector2.zero)
+            {
+
+                bool success = TryMove(movementInput);
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(movementInput.x, 0));
+                }
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+
+                animator.SetBool("isMoving", success);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+
+         
+            if (movementInput.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            
+            }
+            else if (movementInput.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            
+            }
+        }
+    }
+
+    private bool TryMove(Vector2 direction) {
+        if (direction != Vector2.zero)
+        {
+            
+            int count = rb.Cast(
+                direction, 
+                movementFilter, 
+                castCollisions, 
+                moveSpeed * Time.fixedDeltaTime + collisionOffset); 
+
+            if (count == 0)
+            {
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+
+    void OnMove(InputValue movementValue) {
+        movementInput = movementValue.Get<Vector2>();
+    }
+    void OnFire() {
+        animator.SetTrigger("swordAttack");
+    }
+
+    public void SwordAttack()
+    {
+        LockMovement();
+
+        if (spriteRenderer.flipX == true)
+        {
+            swordAttack.AttackLeft();
+        }
+        else
+        {
+            swordAttack.AttackRight();
+        }
+    }
+
+    public void EndSwordAttack() {
+        UnlockMovement();
+        swordAttack.StopAttack();
+    }
+
+    public void LockMovement() {
+        canMove = false;
+    }
+    public void UnlockMovement() {
+        canMove = true;
+    }
+}
---
+++ DIZONDLMOTO/Assets/SwordAttack.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttack : MonoBehaviour
+{
+    public Collider2D swordCollider;
+
+    public float damage = 3;
+
+    Vector2 rightAttackOffset;
+   
+    private void Start()
+    {
+        rightAttackOffset = transform.position;
+    }
+
+  
+    public void AttackRight() {
+        print("RIGHT");
+        swordCollider.enabled = true;
+        transform.localPosition = rightAttackOffset;
+    }
+
+    public void AttackLeft() {
+        print("LEFT");
+        swordCollider.enabled = true;
+        transform.localPosition = new Vector3(rightAttackOffset.x* -1, rightAttackOffset.y);
+    }
+    public void StopAttack() {
+        swordCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Enemy") {
+            Enemy enemy = other.GetComponent<Enemy>();
+
+            if (enemy != null) {
+                enemy.Health -= damage;
+            }
+        }
+    }
+        
+    }
+
+   
+
